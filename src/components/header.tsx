3fe696dcc0712b2f3f7ac0f71c16799910306067
Leonardo Theodoro <simplespinheiro@gmail.com>
2024-04-30 08:38:58 -0300

    ✨ add avatar on logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,17 @@ import { ToggleTheme } from './toggle-theme'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
 export function Header() {
   return (
     <div className="flex items-center justify-between pb-16 pt-4 font-bold">
-      <Link href="/">
+      <Link href="/" className="flex items-center gap-2">
+        <Avatar>
+          <AvatarImage src="https://github.com/leotheodoro.png" />
+          <AvatarFallback>LT</AvatarFallback>
+        </Avatar>
+
         <h1 className="text-2xl">Theodoro.dev</h1>
       </Link>
       <div className="flex items-center gap-4">
