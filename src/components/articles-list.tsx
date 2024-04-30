c5d92b2ae01c78d115a500efce536d7956777a2c
Leonardo Theodoro <simplespinheiro@gmail.com>
2024-04-30 01:16:00 -0300

    :construction: add pagination

--- src/components/articles-list.tsx
+++ src/components/articles-list.tsx
@@ -1,12 +1,44 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination'
 import { articles } from '@/types/article'
 import { ArticleItem } from './article-item'
 
 export function ArticlesList() {
   return (
-    <ul>
-      {articles.map((article) => (
-        <ArticleItem article={article} key={article.id} />
-      ))}
-    </ul>
+    <>
+      <ul>
+        {articles.map((article) => (
+          <ArticleItem article={article} key={article.id} />
+        ))}
+      </ul>
+      <Pagination className="pt-6">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#" isActive>
+              1
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">2</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </>
   )
 }
